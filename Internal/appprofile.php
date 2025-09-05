3974ee62de7fff6e5dfc3fc9f4ffa3b6cc2cd434
SukkaBot <githubbot@skk.moe>
2025-09-05 11:49:59 +0000

    deploy: https://github.com/SukkaW/Surge/commit/f7b92be6aaa0178854f4e268560f55efd03bbf58

--- Internal/appprofile.php
+++ Internal/appprofile.php
@@ -1,6 +1,6 @@
 <?php
 
-// # Build 2025-09-05T05:15:54.841Z
+// # Build 2025-09-05T11:49:32.285Z
 
 declare(strict_types=1);
 
@@ -1202,7 +1202,6 @@ $_ENV['Clash_Group_Config'] = [
         'DOMAIN-KEYWORD,1drv,Microsoft & Apple',
         'DOMAIN-KEYWORD,microsoft,Microsoft & Apple',
         'DOMAIN-KEYWORD,hotmail,Microsoft & Apple',
-        'PROCESS-NAME,trustd,Microsoft & Apple',
         'PROCESS-NAME,netbiosd,Microsoft & Apple',
         'PROCESS-NAME,com.apple.geod,Microsoft & Apple',
         'PROCESS-NAME,mapspushd,Microsoft & Apple',
@@ -5632,10 +5631,6 @@ $_ENV['Clash_Group_Config'] = [
         'IP-CIDR,154.8.128.0/17,Domestic',
         'IP-CIDR,154.72.44.0/24,Domestic',
         'IP-CIDR,154.89.32.0/20,Domestic',
-        'IP-CIDR,154.89.49.0/24,Domestic',
-        'IP-CIDR,154.89.50.0/23,Domestic',
-        'IP-CIDR,154.89.52.0/22,Domestic',
-        'IP-CIDR,154.89.56.0/21,Domestic',
         'IP-CIDR,155.126.176.0/23,Domestic',
         'IP-CIDR,156.107.160.0/24,Domestic',
         'IP-CIDR,156.107.170.0/24,Domestic',
@@ -5719,8 +5714,6 @@ $_ENV['Clash_Group_Config'] = [
         'IP-CIDR,175.16.0.0/13,Domestic',
         'IP-CIDR,175.24.0.0/15,Domestic',
         'IP-CIDR,175.27.0.0/16,Domestic',
-        'IP-CIDR,175.29.107.0/24,Domestic',
-        'IP-CIDR,175.29.108.0/22,Domestic',
         'IP-CIDR,175.30.0.0/15,Domestic',
         'IP-CIDR,175.42.0.0/15,Domestic',
         'IP-CIDR,175.44.0.0/16,Domestic',
